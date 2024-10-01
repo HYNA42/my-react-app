f74d6950297368c55e7786651fa90489fdaede15
Amadou <bylo@duck.com>
2024-10-02 00:59:30 +0200

    commit quete 4

---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,20 @@
+
+
+function PokemonCard({pokemon}) {
+  // console.log(props);
+  // const pokemon = pokemonList[1];
+  // const pokemon = { name: "mew", imgSrc: null };
+
+  return (
+    <figure>
+      {pokemon.imgSrc ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      ) : (
+        <p>???</p>
+      )}
+      <figcaption>{pokemon.name}</figcaption>
+    </figure>
+  );
+}
+
+export default PokemonCard;
