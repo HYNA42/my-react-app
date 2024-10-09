43e8d43f512e5848d75a10798a6f313fcfef931f
Amadou <bylo@duck.com>
2024-10-10 01:10:38 +0200

    commit last

--- src/App.tsx
+++ src/App.tsx
@@ -4,29 +4,29 @@ import PokemonCard from "./components/PokemonCard";
 
 const pokemonList = [
   {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "charmander",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
-    },
-    {
-      name: "squirtle",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
-    },
-    {
-      name: "pikachu",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 function App() {
   const [pokemonIndex, setPokemonIndex] = useState(0);
@@ -39,18 +39,19 @@ function App() {
     setPokemonIndex(pokemonIndex + 1);
   };
 
-
   return (
     <>
       <div>
         <PokemonCard pokemon={pokemonList[pokemonIndex]} />
       </div>
-      
-      {pokemonIndex < (pokemonList.length - 1) ? <button onClick={handleNext}>Suivant</button> : null}
-      
-      {pokemonIndex > 0 ? <button onClick={handlePrevious}>Précédent</button> : null }
-      
-      
+
+      {pokemonIndex < pokemonList.length - 1 ? (
+        <button onClick={handleNext}>Suivant</button>
+      ) : null}
+
+      {pokemonIndex > 0 ? (
+        <button onClick={handlePrevious}>Précédent</button>
+      ) : null}
     </>
   );
 }
